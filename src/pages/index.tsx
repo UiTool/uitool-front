08707cb5f48c776f9472d66f03c64ab216bd10c7
walo2 <wilson.al.oliver@gmail.com>
2022-09-07 20:28:50 -0300

    Criação do layout (parcial)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
 import type { NextPage } from 'next'
+import { Header } from '../components/header'
+import { Footer } from '../components/footer'
 
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <h1 className={styles.title}>Home!</h1>
+    <>
+      <div className={styles.outerContainer}>
+        <Header />
+        <div className={styles.videoContainer}>
+          <div className={styles.videoBox}>
+            <span>VIDEO</span>
+          </div>
+        </div>
+        <div className={styles.videoInfo}>
+          <h2>Lorem Ipsun</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a ligula ipsum. Suspendisse vel nibh nec eros dapibus malesuada.</p>
+        </div>
+      </div>
+
+      <Footer />
+    </>
   )
 }
 
