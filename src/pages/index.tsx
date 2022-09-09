89447ba4431e4683e2e895c1b6e6d9889444ad64
walo2 <wilson.al.oliver@gmail.com>
2022-09-09 17:23:53 -0300

    Adição dos botões à home
    Adição dos botões "Dashboard" e "Logout" à tela inicial.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,29 @@
+import Image from 'next/image'
 import type { NextPage } from 'next'
 import { Header } from '../components/header'
 import { Footer } from '../components/footer'
+import dashboard_button from '../public/dashboard_button.svg'
+import logout_button from '../public/logout_button.svg'
 
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
     <>
+      <Header />
       <div className={styles.outerContainer}>
-        <Header />
-        <div className={styles.videoContainer}>
-          <div className={styles.videoBox}>
-            <span>VIDEO</span>
-          </div>
+        <div className={styles.buttons}>
+          <Image src={dashboard_button} alt="Logo UI Tools" />
+          <Image src={logout_button} alt="Login" />
+        </div>
+        <div className={styles.videoBox}>
+          <span>VÍDEO</span>
         </div>
         <div className={styles.videoInfo}>
           <h2>Lorem Ipsun</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a ligula ipsum. Suspendisse vel nibh nec eros dapibus malesuada.</p>
         </div>
       </div>
-
       <Footer />
     </>
   )
