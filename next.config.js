96b36cdc06d84a84739a6105017396ce85f43372
yuutaDragon <guilherme.rodrigues@estudante.ufscar.br>
2022-09-19 11:00:03 -0300

    Fluxo autenticacao

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // env: {
-  //   BACKEND_URL
-  // }
+  env: {
+    BACKEND_URL: 'http://localhost:4003/'
+  }
 }
 
 module.exports = nextConfig
