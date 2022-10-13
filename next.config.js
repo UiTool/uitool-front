e9fab88b722b2b3dcc15afa0243669aa7e7a46fa
yuutaDragon <guilherme.rodrigues@estudante.ufscar.br>
2022-10-13 12:08:06 -0300

    validation forms

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    BACKEND_URL: 'http://localhost:4003/',
-    GOOGLE_CLIENT_ID: '155056270335-lpmculk3o4dumjk9hkeurnvv9trt56sj.apps.googleusercontent.com',
-    GOOGLE_CLIENT_SECRET: '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'
+    BACKEND_URL,
+    GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET,
   }
 }
 
