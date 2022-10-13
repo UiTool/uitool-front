47c78c610ee2c8e4f9911925511062d491b3d3bd
yuutaDragon <guilherme.rodrigues@estudante.ufscar.br>
2022-10-13 12:23:54 -0300

    validation forms

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    BACKEND_URL,
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-  }
 }
 
 module.exports = nextConfig
