cd2b75079c6297ed7ac4f1c2bf728a7b1f5665ab
yuutaDragon <guilherme.rodrigues@estudante.ufscar.br>
2022-12-06 15:07:56 -0300

    fix

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 import styles from "./styles.module.scss";
 
 import { LoginMenu } from "../LoginButton";
-import logo from "../../../public/logo.svg";
+import logoUiTool from "../../../public/logo.svg";
 import Link from "next/link";
 
 export const Header: React.FC = () => {
+  
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
@@ -14,7 +15,7 @@ export const Header: React.FC = () => {
         <div className={styles.logo}>
           <Link href="/">
             <a>
-              <Image src={logo} alt="Logo UI Tools" />
+              <Image src={logoUiTool} alt="Logo UI Tools" />
             </a>
           </Link>
         </div>
